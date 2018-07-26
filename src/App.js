6ea05de1d21787b20eca3164f2b6b94dc97fa7af
Beth Damato <bethadamato@gmail.com>
2018-07-26 10:55:18 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+        <p className="App-intro">
+          To get started...DON
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
